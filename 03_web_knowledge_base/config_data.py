2e43e95f6194c7d6658c3af93d2155fa345e47f2
Pipiwind <2971970653@qq.com>
2026-08-06 09:50:10 +0800

    Add configuration data for web knowledge base

---
+++ 03_web_knowledge_base/config_data.py
@@ -0,0 +1,12 @@
+
+md5_path = "./ md5.text"
+
+#Croma
+collection_name = "rag"
+persist_directory = "./chroma_db"
+
+#spliter
+chunk_size = 1000
+chunk_overlap = 100
+separators = ["\n\n","\n",",",".","?","!","，","。","？","！"," ",""]
+max_spliter_char_number = 1000       #文本分割的阈值
