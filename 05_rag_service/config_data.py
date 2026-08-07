63283156ef2b96a5e924d3416204470ae6af4268
Pipiwind <2971970653@qq.com>
2026-08-07 10:47:15 +0800

    Create config_data.py with service settings
    Add configuration parameters for the rag service.

---
+++ 05_rag_service/config_data.py
@@ -0,0 +1,18 @@
+
+md5_path = "./ md5.text"
+
+#Croma
+collection_name = "rag"
+persist_directory = "./chroma_db"
+
+#spliter
+chunk_size = 1000
+chunk_overlap = 100
+separators = ["\n\n","\n",",",".","?","!","，","。","？","！"," ",""]
+max_spliter_char_number = 1000       #文本分割的阈值
+
+#相似度检索阈值
+similarity_threshold = 2   # 返回检索值的文档数量
+
+embedding_model_name = "text-embedding-v4"
+chat_model_name = "qwen3-max"
